04c89bf84e97b4a2ed9bc59a086d3f4af7e0aed8
pranta219 <mdpranta219@gmail.com>
2022-05-21 01:08:34 +0600

    add middleware

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(cors())
+app.use(express.json())
