ef474637e74cb959e859ca8d49c1a03f5496d4b5
pranta219 <mdpranta219@gmail.com>
2022-05-21 01:09:36 +0600

    add auth

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ const app = express();
 // middleware
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.conwe.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('woodToy').collection('service')
+
+        // AUTH
+        app.post('/login', (req, res) => {
+            const user = req.body
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            })
+            res.send({ accessToken })
+        })
