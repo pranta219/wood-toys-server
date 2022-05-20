de8953442682cf1891c8445024305f0a20be3542
pranta219 <mdpranta219@gmail.com>
2022-05-21 01:10:19 +0600

    add server

--- index.js
+++ index.js
@@ -29,3 +29,20 @@ async function run() {
             })
             res.send({ accessToken })
         })
+
+
+        // SERVICES API
+        app.get('/service', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+        })
+
